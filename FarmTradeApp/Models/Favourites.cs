7c84bf5637ac787bbfa7286bd9665195d60e6423
plitn <fancaran@gmail.com>
2023-04-06 18:50:56 +0300

    favourites bugs fixed

--- FarmTradeApp/Models/Favourites.cs
+++ FarmTradeApp/Models/Favourites.cs
@@ -1,12 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace FarmTradeApp.Models;
 
 [Table("favourites")]
+[Keyless]
 public class Favourites
 {
-    [Key]
     public int user_id { get; set; }
     public int product_id { get; set; }
 }
